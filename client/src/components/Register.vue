b3a8d92028cb865139e80fb74c277abc84267fb2
TAFAQUH\tafaquh <tafaquh.fiddinal@gmail.com>
2018-03-15 09:27:18 +0700

    Register Form and Sequilize SetUp

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,12 +1,25 @@
 <template>
-  <div>
-    <h1>Register</h1>
-    <input type="email" name="email" v-model="email" placeholder="email" />
-    <br>
-    <input type="password" name="password" v-model="password" placeholder="password" />
-    <br>
-    <button @click="register">Register</button>
-  </div>
+  <v-container grid-list-md text-xs-center>
+    <v-layout row wrap>
+      <v-flex xs6 offset-xs3>
+        <div class="white elevation-2">
+          <v-toolbar flat dense class="cyan" dark>
+            <v-toolbar-title>Register</v-toolbar-title>
+          </v-toolbar>
+
+          <div class="pl-4 pr-4 pt-2 pb-2">
+            <input type="email" name="email" v-model="email" placeholder="email" />
+            <br>
+            <input type="password" name="password" v-model="password" placeholder="password" />
+            <br>
+            <div class="error" v-html="error"></div>
+            <br>
+            <v-btn class="cyan" @click="register">Register</v-btn>
+          </div>
+        </div>
+      </v-flex>
+    </v-layout>  
+  </v-container>  
 </template>
 
 <script>
@@ -15,20 +28,27 @@ export default {
   data () {
     return {
       email: '',
-      password: ''
+      password: '',
+      error: null
     }
   },
   methods: {
     async register () {
-      await AuthenticationService.register({
-        email: this.email,
-        password: this.password
-      })
+      try {
+        await AuthenticationService.register({
+          email: this.email,
+          password: this.password
+        })
+      } catch (error) {
+        this.error = error.response.data.error
+      }
     }
   }
 }
 </script>
 
 <style scoped>
-
+  .error {
+    color: red;
+  }
 </style>
